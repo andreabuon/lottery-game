9e9fc475be79d348e673105b601583afa5fae045
Andrea Buonaurio <andrea.buon@protonmail.com>
2024-08-23 11:18:13 +0200

    Changed order in index.mjs

--- server/index.mjs
+++ server/index.mjs
@@ -3,13 +3,16 @@ import express from 'express';
 
 // init express
 const app = new express();
-const port = 3001;
+const PORT = 3001;
 
-// activate the server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+/*** APIs ***/
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+// Activating the server
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
