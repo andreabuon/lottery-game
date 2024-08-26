eacda33ef4053bb46aaa547c943db71327749b08
Andrea Buonaurio <andrea.buon@protonmail.com>
2024-08-26 16:26:40 +0200

    Modified Links into NavLink into the Navbar

--- client/src/components/NavHeader.jsx
+++ client/src/components/NavHeader.jsx
@@ -1,22 +1,22 @@
 import { Container, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { LogoutButton } from './AuthComponents';
 
 function NavHeader (props) {
   return(
     <Navbar bg='primary' data-bs-theme='dark'>
       <Container fluid>
-        <Link to='/' className='navbar-brand'>Lottery Game</Link>
+        <NavLink to='/' className='navbar-brand'>Lottery Game</NavLink>
         {
           props.loggedIn ? 
-          <Link to='/scoreboard' className="btn btn-outline-light">Scoreboard</Link> :
+          <NavLink to='/scoreboard' className="btn btn-outline-light">Scoreboard</NavLink> :
           <Container/>
         }
         
         {
         props.loggedIn ? 
           <LogoutButton logout={props.handleLogout} /> :
-          <Link to='/login' className='btn btn-outline-light'>Login</Link>
+          <NavLink to='/login' className='btn btn-outline-light'>Login</NavLink>
         }
       </Container>
     </Navbar>
