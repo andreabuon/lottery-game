0fa80b6a79ba1f192da919a118a9bab1eeb4999c
Andrea Buonaurio <andrea.buon@protonmail.com>
2024-08-26 17:48:22 +0200

    Added scoreboard table

--- client/src/components/ScoreboardComponent.jsx
+++ client/src/components/ScoreboardComponent.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import Table from 'react-bootstrap/Table';
+//import APIs
+
+//TODO load top 3 users from db
+function aggiornaLista() {
+    console.log("Click!");
+};
+
 export default function Scoreboard() {
-    return(
-        <h1>Scoreboard</h1> 
+    const [scoreboardList, setScoreboard] = useState([{ username: 'user1', score: 1 }, { username: 'user2', score: 2 }, { username: 'user3', score: 3 }]);
+    useEffect(() => aggiornaLista(), [scoreboardList]);
+
+    return (
+        <>
+            <h1>Scoreboard</h1>
+            <Button onClick={aggiornaLista}>Refresh</Button>
+
+            <Table striped>
+                <thead>
+                    <tr>
+                        <th>Score</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {scoreboardList.map((el) => (
+                        <tr>
+                            <td>{el.score}</td>
+                            <td>{el.username}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </>
     );
 }
