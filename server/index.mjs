769d64a73d53927675212c6fb1247fe67dce694b
Andrea Buonaurio <andrea.buon@protonmail.com>
2024-08-27 18:28:30 +0200

    Added login APIs in the server

--- server/index.mjs
+++ server/index.mjs
@@ -64,10 +64,50 @@ app.use(passport.authenticate('session'));
 
 /*** APIs ***/
 
+// POST /api/sessions
+// This route is used for performing login.
+app.post('/api/sessions', function(req, res, next) {
+  passport.authenticate('local', (err, user, info) => {
+    if (err)
+      return next(err);
+      if (!user) {
+        // display wrong login messages
+        return res.status(401).json({ error: info});
+      }
+      // success, perform the login and extablish a login session
+      req.login(user, (err) => {
+        if (err)
+          return next(err);
+
+        // req.user contains the authenticated user, we send all the user info back
+        // this is coming from userDao.getUserByCredentials() in LocalStratecy Verify Function
+        return res.json(req.user);
+      });
+  })(req, res, next);
+});
+
+// GET /api/sessions/current
+// This route checks whether the user is logged in or not.
+app.get('/api/sessions/current', (req, res) => {
+  if(req.isAuthenticated()) {
+    res.status(200).json(req.user);}
+  else
+    res.status(401).json({error: 'Not authenticated'});
+});
+
+// DELETE /api/session/current
+// This route is used for loggin out the current user.
+app.delete('/api/sessions/current', (req, res) => {
+  req.logout(() => {
+    res.end();
+  });
+});
+
+/*******/
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
 
 // Activating the server
 const PORT = 3001;
