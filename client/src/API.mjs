139f27de83e091f658b91de86538bbcf619b3117
Andrea Buonaurio <andrea.buon@protonmail.com>
2024-08-31 15:16:36 +0200

    Added try/catch in client APIs and other minor fixes

--- client/src/API.mjs
+++ client/src/API.mjs
@@ -1,84 +1,96 @@
 const SERVER_URL = 'http://localhost:3001';
 
-async function handleError(response) {
-  const errorMessage = await response.text();
-  throw new Error(`${response.status} - ${response.statusText}: ${errorMessage}`);
+async function handleInvalidResponse(response) {
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw new Error(`${response.status} - ${response.statusText}: ${errorMessage}`);
+  }
 }
 
 const logIn = async (credentials) => {
-  const response = await fetch(SERVER_URL + '/api/sessions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-    body: JSON.stringify(credentials),
-  });
-  if (!response.ok) {
-    handleError(response);
+  try {
+    const response = await fetch(SERVER_URL + '/api/sessions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(credentials),
+    });
+
+    await handleInvalidResponse(response);
+    const user = await response.json();
+    return user;
+  } catch (error) {
+    throw new Error(error)
   }
-  const user = await response.json();
-  return user;
 };
 
 const getUserInfo = async () => {
-  const response = await fetch(SERVER_URL + '/api/sessions/current', {
-    credentials: 'include',
-  });
-  if (!response.ok) {
-    handleError(response);
+  try {
+    const response = await fetch(SERVER_URL + '/api/sessions/current', { credentials: 'include' });
+    await handleInvalidResponse(response);
+    const user = await response.json();
+    return user;
+  } catch (error) {
+    throw new Error(error);
   }
-  const user = await response.json();
-  return user;
 };
 
 const logOut = async () => {
-  const response = await fetch(SERVER_URL + '/api/sessions/current', {
-    method: 'DELETE',
-    credentials: 'include'
-  });
-  if (!response.ok) {
-    handleError(response);
+  try {
+    const response = await fetch(SERVER_URL + '/api/sessions/current', {
+      method: 'DELETE',
+      credentials: 'include'
+    });
+    await handleInvalidResponse(response);
+    return null;
+  } catch (error) {
+    throw new Error(error);
   }
-  return null;
 };
 
 const getBestScores = async () => {
-  const response = await fetch(SERVER_URL + '/api/scores/best', {
-    credentials: 'include'
-  });
-  if (!response.ok) {
-    handleError(response);
+  try {
+    const response = await fetch(SERVER_URL + '/api/scores/best', {
+      credentials: 'include'
+    });
+    await handleInvalidResponse(response);
+    const scores = await response.json();
+    return scores;
+  } catch (error) {
+    throw new Error(error);
   }
-  const scores = await response.json();
-  return scores;
 };
 
 const getLastDraw = async () => {
-  const response = await fetch(SERVER_URL + '/api/draws/last', {
-    credentials: 'include'
-  });
-
-  if (!response.ok) {
-    handleError(response);
+  try {
+    const response = await fetch(SERVER_URL + '/api/draws/last', {
+      credentials: 'include'
+    });
+    await handleInvalidResponse(response);
+    const draw = await response.json();
+    return draw;
+  } catch (error) {
+    throw new Error(error);
   }
-  const draw = await response.json();
-  return draw;
-};
+}
 
 const createBet = async (bet) => {
-  const response = await fetch(SERVER_URL + '/api/bets/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-    body: JSON.stringify(bet)
-  });
-  if (!response.ok) {
-    handleError(response);
+  try {
+    const response = await fetch(SERVER_URL + '/api/bets/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(bet)
+    });
+    await handleInvalidResponse(response);
+    return null;
+  } catch (error) {
+    throw new Error(error);
   }
-  return; //FIXME
 };
 
 const API = { logIn, getUserInfo, logOut, getBestScores, getLastDraw, createBet };
