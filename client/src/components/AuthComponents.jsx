1406832178fb4550401581a9014b41a638dcc0a1
Andrea Buonaurio <andrea.buon@protonmail.com>
2024-09-01 10:08:49 +0200

    Added logout icon

--- client/src/components/AuthComponents.jsx
+++ client/src/components/AuthComponents.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { MdLogout } from "react-icons/md";
 
 function LoginForm(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  
+
   const handleSubmit = (event) => {
-      event.preventDefault();
-      
-      const credentials = { username, password };
-      
-      props.login(credentials);
+    event.preventDefault();
+
+    const credentials = { username, password };
+
+    props.login(credentials);
   };
 
   return (
@@ -19,26 +20,26 @@ function LoginForm(props) {
       <Col md={6}>
         <Form onSubmit={handleSubmit}>
           <Form.Group controlId='username' className='mb-3'>
-              <Form.Label>Username</Form.Label>
-              <Form.Control type='text' value={username} onChange={ev => setUsername(ev.target.value)} required={true} />
+            <Form.Label>Username</Form.Label>
+            <Form.Control type='text' value={username} onChange={ev => setUsername(ev.target.value)} required={true} />
           </Form.Group>
 
           <Form.Group controlId='password' className='mb-3'>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type='password' value={password} onChange={ev => setPassword(ev.target.value)} required={true} minLength={6}/>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password' value={password} onChange={ev => setPassword(ev.target.value)} required={true} minLength={6} />
           </Form.Group>
 
           <Button type='submit'>Login</Button>
           <Link className='btn btn-danger mx-2 my-2' to={'/'} >Cancel</Link>
-      </Form>
-    </Col>
-  </Row>
+        </Form>
+      </Col>
+    </Row>
   )
 };
 
 function LogoutButton(props) {
-  return(
-    <Button variant='outline-light' onClick={props.logout}>Logout</Button>
+  return (
+    <Button variant='outline-light' onClick={props.logout}><MdLogout className='me-2' />Logout</Button>
   )
 }
 
