775484e02d397c9c887e0f673052d8b574da49ff
Andrea Buonaurio <andrea.buon@protonmail.com>
2024-09-01 09:15:37 +0200

    Added checks in the Bet constructor

--- common/Bet.mjs
+++ common/Bet.mjs
@@ -1,11 +1,26 @@
 import { Draw } from './Draw.mjs';
 
+const BET_MAX_SIZE = 3;
 const COST_PER_NUMBER = 5; // pts
 const PTS_PER_MATCHED_NUM = 10;
+const MAX_NUM = 90;
+const MIN_NUM = 1;
 
-export function Bet(user_id, numbers) {
+
+export function Bet(user_id, nums) {
   this.user_id = user_id;
-  this.numbers = new Set(numbers);
+  this.numbers = new Set();
+  for (const num of nums) {
+    if (num <= MAX_NUM && num >= MIN_NUM) {
+      this.numbers.add(num)
+    }
+  }
+  if (this.getSize == 0) {
+    throw Error('Bet size cannot be 0');
+  }
+  if (this.getSize > BET_MAX_SIZE) {
+    throw Error('Bet size cannot be greater than ', BET_MAX_SIZE);
+  }
 }
 
 Bet.prototype.getSize = () => {
