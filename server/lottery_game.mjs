763e06e57841c8f46c51462caacaaf707f276d7e
Andrea Buonaurio <andrea.buon@protonmail.com>
2024-09-10 14:22:47 +0200

    Split updateScores() in multiple functions

--- server/lottery_game.mjs
+++ server/lottery_game.mjs
@@ -14,55 +14,84 @@ export async function createBet(req_user, numbers) {
 
     let round = await getRound();
     const bet = new Bet(round, user.user_id, numbers);
+
     const cost = bet.getCost();
     if (user.score < cost) {
       throw new Error(`The player ${user.user_id} does not have enough points to place this bet.`);
     }
 
     await addBet(bet);
+
     // Subtract bet cost from user score
     await updateUserScore(user.user_id, user.score - cost);
-    console.log(`[Round ${bet.round}] Player ` + + bet.user_id + " made a new bet [" + bet.numbers + "] in the DB.");
+    console.log(`[Round ${bet.round}] Player ` + + bet.user_id + " made a new bet [" + bet.numbers + "].");
     return bet;
   } catch (error) {
-    //console.error(`Error while creating bet for the player #${req_user.user_id}: ${error}`);
+    console.error(`Error while creating bet for the player #${req_user.user_id}: ${error}`);
     throw error;
   }
 }
 
-export async function updateScores(round) {
+async function addReward(user_id, reward, round) {
   try {
-    const draw = await getDrawByRound(round);
-
-    const bets = await getRoundBets(round);
-    console.log(`[Round ${round}] Bets: `);
-    if (!bets | bets.length == 0) {
-      console.log(`[Round ${round}] No bets found.`);
+    const user = await getUserById(user_id);
+    if (!user) {
+      console.error(`[Round ${round}] The user ${user_id} has not been found - skipping its score update!`);
+      return;
     }
+    await updateUserScore(user.user_id, user.score + reward);
+    //console.log(`Updated Player ${user.username} score: previus score = ${user.score} reward: ${reward} - new score: ${user.score+reward}`);
+  } catch (error) {
+    console.error(`[Round ${round}] Error managing user ${user_id} score: `, error);
+  }
+}
 
-    for (let bet of bets) {
-      const reward = bet.computeReward(draw);
-      console.log(`[Round ${round}] ${bet.toString()} and won ${reward} points.`);
-      await addResult(round, bet.user_id, reward)
-      if (reward === 0) {
-        continue;
-      }
-      try {
-        const user = await getUserById(bet.user_id);
-        if (!user) {
-          console.error(`[Round ${round}] The user ${bet.user_id} has not been found - skipping its score update!`);
-          continue;
-        }
-        await updateUserScore(user.user_id, user.score + reward);
-        //console.log(`Updated Player ${user.username} score: previus score = ${user.score} reward: ${reward} - new score: ${user.score+reward}`);
-      } catch (error) {
-        console.error(`[Round ${round}] Error computing user ${bet.user_id} score:`, error);
-      }
+async function processBet(bet, draw, round) {
+  const reward = bet.computeReward(draw);
+  console.log(`[Round ${round}] ${bet.toString()} and won ${reward} points.`);
+
+  try {
+    await addResult(round, bet.user_id, reward)
+
+    if (reward > 0) {
+      await addReward(bet.user_id, reward, round);
     }
   } catch (error) {
-    console.error(`[Round ${round}] Error updating scores:`, error);
+    console.error(`[Round ${round}] Error processing user ${bet.user_id} bet: `, error);
   }
-  console.log(`[Round ${round}] Scores updated. \n`);
+}
+
+async function manageBets(round) {
+  try {
+    const draw = await getDrawByRound(round);
+    const bets = await getRoundBets(round);
+
+    console.log(`[Round ${round}] Bets: `);
+    if (!bets || bets.length == 0) {
+      console.log(`[Round ${round}] No bets found.`);
+      return;
+    }
+
+    for (const bet of bets) {
+      await processBet(bet, draw, round);
+    }
+  } catch (error) {
+    console.error(`[Round ${round}] Error managing bets:`, error);
+  }
+}
+
+async function endRound(round) {
+  try {
+    console.log(`[Round ${round}] Round completed.`);
+    const draw = pickDraw(); // Create a new draw
+    await addDraw(round, draw); // Save draw to the database
+    console.log(`[Round ${round}] ` + "Added draw " + draw.toString() + " in the DB");
+
+    await manageBets(round);
+    console.log(`[Round ${round}] Scores updated. \n`);
+  } catch (error) {
+    console.error(`Error ending round ${round}: ${error}`);
+  }
 }
 
 async function newRound() {
@@ -72,16 +101,8 @@ async function newRound() {
     console.log(`[Round ${round}] A new round has started. 🆕`);
     setTimeout(() => endRound(round), ROUNDS_TIMEOUT);
   } catch (error) {
-    console.error(`Error running round: ${error}`);
+    console.error(`Error running new round: ${error}`);
   }
-}
-
-async function endRound(round) {
-  console.log(`[Round ${round}] Round completed.`);
-  const draw = pickDraw(); // Create a new draw
-  await addDraw(round, draw); // Save draw to the database
-  console.log(`[Round ${round}] ` + "Added draw " + draw.toString() + " in the DB");
-  await updateScores(round);
 }
 
 export async function runGame() {
